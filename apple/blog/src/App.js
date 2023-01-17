22032228615b552dcafa075f332440f2a6dffb5d
ASH\안수현 <ash199358@gmail.com>
2023-01-17 22:24:44 +0900

    no message

--- apple/blog/src/App.js
+++ apple/blog/src/App.js
@@ -6,6 +6,8 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
+  let [titleNo, setTitleNo] = useState(0);
+  let [입력값, 입력값변경] = useState('');
 
   return (
     <div className="App">
@@ -16,9 +18,9 @@ function App() {
       {
         글제목.map(function (a, i) {
           return (
-            <div className="list" key={i}>
-              <h4>{a} {/**=====또는  { 글제목[i] } </h4> */}
-                <span onClick={() => { /**======= 리스트별로 따봉 따로해보기 */
+            <div className="list" key={i} >
+              <h4 onClick={() => { setModal(true); setTitleNo(i); }}>{a} {/**===== 1. state변경 */}
+                <span onClick={() => {
                   let copy = [...따봉];
                   copy[i] = copy[i] + 1;
                   따봉변경(copy)
@@ -26,13 +28,14 @@ function App() {
               </h4>
               <p>2월 18일 발행</p>
             </div>
+
           )
         })
       }
 
-      { /** ===== setModal 에 따라 보이고 안보이고  */
-        modal ?
-          <Modal></Modal>
+      {
+        modal == true ?
+          <Modal titleNo={titleNo} 글제목={글제목} /> /**===== 2. state변경되면서 탐 & props로 넘겨줌 */
           :
           null
       }
@@ -43,10 +46,10 @@ function App() {
 
 }
 
-function Modal() {
+function Modal(props) { /**===== 3. props 사용 */
   return (
     <div className="modal">
-      <h4>제목</h4>
+      <h4>{props.글제목[props.titleNo]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
