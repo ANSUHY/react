f3409db63bc4a3347e6499da6d1e2e9a75121ddf
ASH\안수현 <ash199358@gmail.com>
2023-02-14 23:06:04 +0900

    no message

--- ash-training-tabulator/src/component/BaseTabulator.js
+++ ash-training-tabulator/src/component/BaseTabulator.js
@@ -1,9 +1,15 @@
 
-import { useRef } from 'react';
-import { ReactTabulator } from 'react-tabulator'
+import { React, useRef } from 'react';
+import { reactFormatter, ReactTabulator, ReactTabulatorOptions } from 'react-tabulator'
 
 /** ===== 기본기본 */
 function BaseTabulator() {
+
+    function SimpleButton(props) {
+        //const rowData = props.cell._cell.row.data;
+        //const cellValue = props.cell._cell.value || 'Edit | Show';
+        //return <button onClick={() => alert(rowData.name)}>{cellValue}</button>;
+    }
 
     const columns = [
         { title: "Name", field: "name", width: 150 },
@@ -11,7 +17,8 @@ function BaseTabulator() {
         { title: "Favourite Color", field: "col" },
         { title: "Date Of Birth", field: "dob", hozAlign: "center" },
         { title: "Rating", field: "rating", hozAlign: "center", formatter: "star" },
-        { title: "Passed?", field: "passed", hozAlign: "center", formatter: "tickCross" }
+        { title: "Passed?", field: "passed", hozAlign: "center", formatter: "tickCross" },
+        { title: 'Custom', field: 'custom', hozAlign: 'center', editor: 'input', formatter: reactFormatter(<SimpleButton />) }
     ];
 
     var data = [
@@ -28,19 +35,20 @@ function BaseTabulator() {
         movableColumns: true
     };
 
-    let ref = useRef();
+    //let ref11 = React.useRef();
+    let ref11 = useRef();
 
     /** === 클릭 이벤트 */
     const rowClickHandler = (e, row) => {
         console.log("aaaaaa")
-        console.log("~~~~~~~~~~")
+
     };
 
     return (
         <>
             <div>
                 <ReactTabulator
-                    onRef={(ref) => (ref = ref)}
+                    onRef={(ref) => (ref11.current = ref)}
                     data={data}
                     columns={columns}
                     tooltips={true}
